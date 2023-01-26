f794c68434632970923ed733ca0cb78d2ea08ac9
sicily <121409961+seanhobeck@users.noreply.github.com>
2023-01-25 22:47:27 -0500

    QOL Optimizations
    - Everything now calls back to vnformat.

--- include/format.h
+++ include/format.h
@@ -17,37 +17,18 @@
 
 /// @brief Namespace for the "gnu/linux c++2a standard".
 namespace __gnu::cxx2a {
-    /// @brief Formats a string.
+    /// @brief Formats a string (with a specified length).
     /// @tparam pargs_t A template for packed arguments (no va_args).
     /// @param __format The string that is going to be formatted.
+    /// @param __n The length of characters you want to format.
     /// @param ...__args Virtual packed arguments to format.
     /// @return Returns a formatted string if the length isn't 0, otherwise it returns a empty string.
-    template<typename ... pargs_t>
-    static const std::string 
-    vformat(const std::string& __format, pargs_t... __args) noexcept
-    {
-        /// Checking the length of the string.
-        if(auto sz = (std::size_t) std::snprintf( nullptr, 0, __format.c_str(), __args...) + 1; sz > 0)
-        {
-            /// Initialize a temporary buffer.
-            std::unique_ptr<char[]> buf(new char[sz]);
-
-            /// Use std::snprintf() and return the string.
-            std::snprintf(buf.get(), sz, ))format.c_str(), __args...);
-            return std::string(buf.get(), buf.get() + sz - 1);
-        }
-
-        ///  Return a empty string.
-        return std::string();
-    };
-    /// @brief Formats a string (with a specified length).
-    /// @param __n The length of characters you want to format.
     template<typename ... pargs_t>
     static const std::string 
     vnformat(const std::string& __format, std::size_t __n, pargs_t... __args) noexcept
     {
         /// Checking the length of the string.
-        if(auto sz = (std::size_t) n; __format.size() > 0 && sz != 0)
+        if(auto sz = (std::size_t) std::snprintf( nullptr, 0, __format.c_str(), __args...) + 1; sz > 0 && n != 0)
         {
             /// Initialize a temporary buffer.
             std::unique_ptr<char[]> buf(new char[n]);
@@ -59,6 +40,18 @@ namespace __gnu::cxx2a {
 
         ///  Return a empty string.
         return std::string();
+    };
+    /// @brief Formats a string.
+    /// @tparam pargs_t A template for packed arguments (no va_args).
+    /// @param __format The string that is going to be formatted.
+    /// @param ...__args Virtual packed arguments to format.
+    /// @return Returns a formatted string if the length isn't 0, otherwise it returns a empty string.
+    template<typename ... pargs_t>
+    static const std::string 
+    vformat(const std::string& __format, pargs_t... __args) noexcept
+    {
+        /// Wrapping function for optimization.
+        return vnformat(__format, (std::size) __format.size(), __args);
     };
     /// @brief Formats a string (with std::optional).
     /// @return Returns a std::optional of the formatted string.
