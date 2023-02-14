8700068aff13bfb1702e18200d09c4ceb8a1bcdc
sicily <121409961+seanhobeck@users.noreply.github.com>
2023-02-13 23:26:13 -0500

    Update flat_map.h
    - Now added the extension to the Flat map, the Flat multimap.

--- src/flat_map.h
+++ src/flat_map.h
@@ -22,7 +22,7 @@ namespace __gnu_cxx::v2a {
     /// @tparam kv_it Key-value iterator.
     /// @tparam kv_cit Key-value const iterator.
     template <typename k, typename v,
-        typename k_map = std::map<k, std::int64_t>,
+        typename k_map = std::map<k, std::size_t>,
         typename v_container = std::vector<v>,
         typename kv_it = v_container::iterator,
         typename kv_cit = v_container::const_iterator>
@@ -66,7 +66,7 @@ namespace __gnu_cxx::v2a {
         /// @brief Get the size of the flat map container.
         /// @return The size of the flat map container.
         std::size_t size() const {
-            return _map.size();
+            return _vec.size();
         };
 
 
@@ -100,13 +100,99 @@ namespace __gnu_cxx::v2a {
 
         ///------------- @section Operator functions. -------------///
 
-
+    
         const v& operator[](const k& _k) const {
             return _vec[_map.at(_k)];
         };
-
         v& operator[](const k& _k) const {
             return _vec[_map.at(_k)];
+        };
+    };
+
+    /// @brief Flat multi map container.
+    /// @tparam k Key typename.
+    /// @tparam v Value typename.
+    /// @tparam k_map Key map typename.
+    /// @tparam v_container Value vector typename.
+    /// @tparam kv_it Key-value iterator.
+    /// @tparam kv_cit Key-value const iterator.
+    template <typename k, typename v,
+        typename k_map = std::map<k, std::vector<std::size_t>>,
+        typename v_container = std::vector<v>,
+        typename kv_it = v_container::iterator,
+        typename kv_cit = v_container::const_iterator>
+    class flat_multi_map {
+    private:
+
+        /// Key-index map.
+        k_map _map;
+        /// Values container.
+        v_container _vec;
+
+    public:
+
+        flat_multi_map() = default;
+
+
+        ///------------- @section Member functions. -------------///
+
+
+        /// @brief Inserting a key and value pair into the multi map.
+        /// @param _k Key
+        /// @param _v Value
+        void insert(const k& _k, const v& _v) {
+            _map[_k].emplace_back(_vec.size());
+            _vec.emplace_back(_v);
+        };
+
+        /// @brief Get the count of the flat map container.
+        /// @return The count of the flat map container.
+        std::size_t count() const {
+            auto it = _map.find(key);
+            return it == _map.end() ? 0 : it->second.size();
+        };
+
+
+        ///------------- @section Container functions. -------------///
+
+
+        /// @brief Get the begin iterator of the flat multi map container.
+        /// @return The begin iterator.
+        kv_it begin() const {
+            return _vec.begin();
+        };
+
+        /// @brief Get the end iterator of the flat multi map container.
+        /// @return The ending iterator.
+        kv_it end() const {
+            return _vec.end();
+        };
+        
+        /// @brief Get the constant begin iterator of the flat multi map container.
+        /// @return The begin iterator.
+        kv_cit cbegin() const {
+            return _vec.cbegin();
+        };
+
+        /// @brief Get the constant end iterator of the flat multi map container.
+        /// @return The ending iterator.
+        kv_cit cend() const {
+            return _vec.cend();
+        };
+
+
+        ///------------- @section Operator functions. -------------///
+
+
+        std::vector<v> operator[](const k& _k) const {
+            std::vector<v> _r;
+            
+            if (auto it = _map.find(_k); it != _map.end()) {
+                for (std::size_t idx : it->second)
+                    _r.emplace_back(_vec[idx]);
+            }
+
+            return _r;
         };
     };
 };
